fa331790b8021439a8a2468b6e73ab2f255eeb1a
Maxim Kuzmin <maxim.kuzmin@inbox.ru>
2024-11-29 09:03:54 +0300

    -

--- Dummy/src/Backend/src/Gateway/src/Apps/WebApp/Program.cs
+++ Dummy/src/Backend/src/Gateway/src/Apps/WebApp/Program.cs
@@ -1,1 +1,34 @@
-﻿await Makc2024.Dummy.Gateway.Apps.WebApp.App.AppHost.RunAsync(args);
+﻿var logger = AppLogger.Create<Program>();
+
+try
+{
+  logger.LogInformation("Starting web application");
+
+  AppEnvironment.LoadVariables();
+
+  var builder = WebApplication.CreateBuilder(args);
+
+  var appConfigSection = builder.Configuration.GetSection(AppConfigOptions.SectionKey);
+
+  var appConfigOptions = appConfigSection.CreateAppConfigOptions();
+
+  builder.Services
+    .AddAppUILayer(logger, appConfigOptions)
+    .AddAppInfrastructureLayer(logger, appConfigOptions, builder.Configuration, appConfigSection);
+
+  var app = builder.Build();
+
+  app.UseAppUILayer(logger);
+
+  app.Run();
+}
+catch (Exception ex)
+{
+  logger.LogCritical(ex, "Application terminated unexpectedly");
+}
+finally
+{
+  AppLogger.CloseAndFlush();
+}
+
+public partial class Program { }
--- Dummy/src/Backend/src/Writer/src/Apps/WebApp/Program.cs
+++ Dummy/src/Backend/src/Writer/src/Apps/WebApp/Program.cs
@@ -1,1 +1,36 @@
-﻿await Makc2024.Dummy.Writer.Apps.WebApp.App.AppHost.RunAsync(args);
+﻿var logger = AppLogger.Create<Program>();
+
+try
+{
+  logger.LogInformation("Starting web application");
+
+  AppEnvironment.LoadVariables();
+
+  var builder = WebApplication.CreateBuilder(args);
+
+  var appConfigSection = builder.Configuration.GetSection(AppConfigOptions.SectionKey);
+
+  var appConfigOptions = appConfigSection.CreateAppConfigOptions();
+
+  builder.Services
+    .AddAppUILayer(logger, appConfigOptions)
+    .AddAppInfrastructureLayer(logger, appConfigOptions, builder.Host, builder.Configuration, appConfigSection);
+
+  var app = builder.Build();
+
+  app.UseAppUILayer(logger);
+
+  await app.UseAppInfrastructureLayerAsync(logger);
+
+  app.Run();
+}
+catch (Exception ex)
+{
+  logger.LogCritical(ex, "Application terminated unexpectedly");
+}
+finally
+{
+  AppLogger.CloseAndFlush();
+}
+
+public partial class Program { }
